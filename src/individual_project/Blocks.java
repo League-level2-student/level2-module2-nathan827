8e8bbf85ad623f521db7747d0333cf885ff64a4f
student <student@ip-172-31-61-150.us-west-2.compute.internal>
2021-06-03 17:57:04 -0700

    a

--- src/individual_project/Blocks.java
+++ src/individual_project/Blocks.java
@@ -1,12 +1,28 @@
 package individual_project;
 
 import javax.swing.*;
+import java.awt.Dimension;
 
 import java.util.*;
 
-public class Blocks {
-	JFrame frame = new JFrame();
-	JButton[] buttons = new JButton [25];
-	JPanel panel = new JPanel();
+public class Blocks{
+	JButton[] buttons = new JButton [100];
+	public void setup() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setVisible(true);
+		frame.add(panel);
+		for(int i = 0; i < buttons.length; i++) {
+			buttons [i] = new JButton();
+			buttons [i].setPreferredSize(new Dimension(50,50));
+			
+			panel.add(buttons [i]);
+		}
+		Dimension size = new Dimension(550,550);
+		panel.setPreferredSize(size);
+		frame.setPreferredSize(size);;
+		frame.pack();
+	}
+	
 	
 }
--- src/individual_project/MineSweeper.java
+++ src/individual_project/MineSweeper.java
@@ -3,6 +3,7 @@ package individual_project;
 
 public class MineSweeper {
 	public static void main(String [] args) {
-		
+		Blocks start = new Blocks();
+		start.setup();
 	}
 }
